38eb79814f9c7d45e8a82fcde35f5c8440dd09eb
sumirpv <sumirpv@gmail.com>
2018-04-20 18:18:22 -0700

    added

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ const PORT = process.env.PORT || 3001;
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 // Serve up static assets
-app.use(express.static("client/build"));
+app.use(express.static("./client/build"));
 // Add routes, both API and view
-app.use(routes);
+ app.use(routes);
 
 
 // Set up promises with mongoose
